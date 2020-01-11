42507c64908ff2502c924ade7e5803b8c5993f42
mariusjagminas <mariusjagminas.it@gmail.com>
2020-01-11 08:44:49 +0100

    Add tests for <NavBar/>

---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { checkIfMatchSnapshot } from "../../utils/tests";
+import { shallow } from "enzyme";
+import NavBar from "./NavBar";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import SearchBox from "../SearchBox/SearchBox";
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+describe("<NavBar/>", () => {
+  const props = {
+    onSearchSubmit: jest.fn(),
+    loading: false
+  };
+  const wrapper = shallow(<NavBar {...props} />).dive();
+
+  it("should render the YouTube icon", () => {
+    expect(wrapper.find(YouTubeIcon)).toHaveDisplayName("YouTubeIcon");
+  });
+
+  it("should render an app title", () => {
+    expect(wrapper.contains("Youtube Videos")).toBeTruthy();
+  });
+
+  it("should render an input element", () => {
+    expect(wrapper.find(SearchBox)).toHaveProp("onSearchSubmit");
+  });
+
+  it("should not render the loading bar if loading={false}", () => {
+    expect(wrapper.find(LinearProgress)).not.toExist();
+  });
+
+  it("should render the loading bar if loading={true}", () => {
+    const wrapper = shallow(
+      <NavBar onSearchSubmit={jest.fn()} loading={true} />
+    ).dive();
+    expect(wrapper.find(LinearProgress)).toExist();
+  });
+
+  checkIfMatchSnapshot(<NavBar {...props} />);
+});
