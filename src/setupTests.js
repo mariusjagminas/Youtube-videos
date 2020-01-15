7dab9a33dfde30d530a7ec0f44d9b1ba4f40f604
mariusjagminas <mariusjagminas.it@gmail.com>
2020-01-15 09:24:48 +0100

    Update Jest setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
+// to fix material-ui error message, when use enzyme's render method
+import React from "react";
+React.useLayoutEffect = React.useEffect;
+//
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "jest-enzyme";
